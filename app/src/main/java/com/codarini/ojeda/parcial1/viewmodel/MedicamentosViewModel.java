a35ec517c7e45a1fab9aaa53fa0f8b2c89a0f910
Brian <brianojd@digip.com.ar>
2025-10-08 23:32:17 -0300

    feat: add Medicamentos VM

--- app/src/main/java/com/codarini/ojeda/parcial1/viewmodel/MedicamentosViewModel.java
+++ app/src/main/java/com/codarini/ojeda/parcial1/viewmodel/MedicamentosViewModel.java
@@ -3,26 +3,29 @@ package com.codarini.ojeda.parcial1.viewmodel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
+import java.util.ArrayList;
+import java.util.List;
 import com.codarini.ojeda.parcial1.data.MedicacionRepository;
 import com.codarini.ojeda.parcial1.model.MedicamentoItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MedicamentosViewModel extends ViewModel {
-    private final MedicacionRepository repo = new MedicacionRepository();
-    private final MutableLiveData<List<MedicamentoItem>> _items = new MutableLiveData<>();
-    public LiveData<List<MedicamentoItem>> items = _items;
 
-    public void loadNoIniciadas(long usuarioId) {
-        //_items.setValue(repo.getNoIniciadasHoy(usuarioId));
+    private final MedicacionRepository repo = new MedicacionRepository();
+
+    private final MutableLiveData<List<MedicamentoItem>> _medicamentos =
+            new MutableLiveData<>(new ArrayList<>());
+    public LiveData<List<MedicamentoItem>> medicamentos = _medicamentos;
+
+    public void cargarNoIniciadas(long usuarioId) {
+        _medicamentos.setValue(repo.obtenerNoIniciadasHoy(usuarioId));
     }
 
-    public void addMockItem() {
-        List<MedicamentoItem> list = _items.getValue();
-        if (list == null) list = new ArrayList<>();
-        list.add(new MedicamentoItem("Nuevo", "16:00", "Comprimido", 250.0, "mg", 1.0, "Agregado dinámico"));
-        _items.setValue(list);
+
+    public void agregarMock() {
+        List<MedicamentoItem> lista = _medicamentos.getValue();
+        if (lista == null) lista = new ArrayList<>();
+        lista.add(new MedicamentoItem("Nuevo medicamento", "Comprimido", 250.0, "mg",
+                1.0, "16:00", "Agregado dinámicamente"));
+        _medicamentos.setValue(lista);
     }
 }
