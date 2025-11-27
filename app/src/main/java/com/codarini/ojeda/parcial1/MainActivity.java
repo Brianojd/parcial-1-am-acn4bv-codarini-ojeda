562e35720409db08326dcfb84279bf519c18924c
Brian <brianojd@digip.com.ar>
2025-11-26 23:54:05 -0300

    feat: manifest login y MainActivity

--- app/src/main/java/com/codarini/ojeda/parcial1/MainActivity.java
+++ app/src/main/java/com/codarini/ojeda/parcial1/MainActivity.java
@@ -1,12 +1,15 @@
 package com.codarini.ojeda.parcial1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.codarini.ojeda.parcial1.ui.home.HomeActivity;
+import com.codarini.ojeda.parcial1.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Opción 1: SIN layout en pantalla (más rápido)
+        // setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            // -------------------------
+            // Usuario logueado
+            // -------------------------
+            startActivity(new Intent(this, HomeActivity.class));
+        } else {
+            // -------------------------
+            // Usuario NO logueado
+            // -------------------------
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        // Cerramos esta Activity para que no vuelva atrás
+        finish();
     }
 }
